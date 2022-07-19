6d1cc40af9268c2e68594544e4d2847cdf384465
PedroMagCoder <pedromagno999@gmail.com>
2022-07-19 20:01:09 -0300

    execicio da aula 7 Pedro Magno

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+
+
+// let nome 
+// let idade 
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// não definidos
+
+//--------------------------------//
+
+const nome = prompt("Qual o teu nome?")
+let idade = + prompt("Qual a tua idade?")
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log("olá" , nome , "você tem" , idade , "anos") ;
+
---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 25;
+
+let NovoA = b;
+let NovoB = a;
+
+console.log("o novo valor de a é" , NovoA , "!!!");
+console.log("o novo valor de b é" , NovoB , "!!!");
